6a27f7681c2cda75f1eec5f1267fff79061af079
junmo-l <wlq1502@gmail.com>
2022-09-18 16:51:38 +0900

    header 부트스트랩 컴포넌트 연결

--- project_shop/src/main.js
+++ project_shop/src/main.js
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import BootstrapVue3 from "bootstrap-vue-3";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
+
 import headerT from "@/components/mainPage/headerT";
 import sectionB from "@/components/mainPage/sectionB";
 import articleB from "@/components/mainPage/articleB";
@@ -14,4 +18,4 @@ app.component("headerT", headerT);
 app.component("sectionB", sectionB);
 app.component("articleB", articleB);
 app.component("footerB", footerB);
-app.use(store).use(router).mount("#app");
+app.use(store).use(BootstrapVue3).use(router).mount("#app");
